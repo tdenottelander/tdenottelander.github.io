54d95ad11e70df7ec07025b654f27cc03bbe6f28
tdenottelander <tdenottelander@gmail.com>
2024-03-13 21:12:07 +0100

    Add Animate-On-Scroll library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core'
+import { AfterViewInit, Component, OnInit } from '@angular/core'
+import AOS from 'aos'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, AfterViewInit {
+  ngOnInit(): void {
+    AOS.init()
+  }
+
+  ngAfterViewInit(): void {
+    // Fixes bug where AOS doesn't start until e.g. moving the browser window.
+    // See https://github.com/michalsnik/aos/issues/807
+    setTimeout(() => {
+      AOS.refresh()
+    }, 500)
+  }
+}
